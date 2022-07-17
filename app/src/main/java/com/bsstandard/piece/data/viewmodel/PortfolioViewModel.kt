a2ff517825c72c9bc134146e322edea6fad6de77
장현민 <wkd556@buysellstandards.com>
2022-07-17 17:05:55 +0900

    포트폴리오 조회 status - PRS 작업 완료 date formatter 적용 완료 ( nn.nn(요일) nn:nn 오픈 )

--- app/src/main/java/com/bsstandard/piece/data/viewmodel/PortfolioViewModel.kt
+++ app/src/main/java/com/bsstandard/piece/data/viewmodel/PortfolioViewModel.kt
@@ -2,8 +2,9 @@ package com.bsstandard.piece.data.viewmodel
 
 import android.annotation.SuppressLint
 import android.app.Application
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Build
-import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.RequiresApi
@@ -11,6 +12,7 @@ import androidx.databinding.BindingAdapter
 import androidx.lifecycle.AndroidViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bsstandard.piece.R
 import com.bsstandard.piece.data.dto.portfolio.PortfolioDTO
 import com.bsstandard.piece.data.repository.PortfolioRepository
 import com.bsstandard.piece.view.adapter.portfolio.PortfolioAdapter
@@ -21,8 +23,10 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.time.DayOfWeek
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.util.*
 
 
 /**
@@ -43,6 +47,7 @@ class PortfolioViewModel(application: Application) : AndroidViewModel(applicatio
     private var portfolioAdapter = PortfolioAdapter(this)
     private val portfolioList: ArrayList<PortfolioDTO.Data.Datum> = arrayListOf()
     var n = 1
+
 
     @SuppressLint("CheckResult")
     fun getPortfolio() {
@@ -85,30 +90,68 @@ object BindingAdapter {
             .into(imageView)
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    @BindingAdapter("app:recruitmentBeginDate")
+    @BindingAdapter("app:recruitmentState")
     @JvmStatic
-    fun loadData(textView: TextView, date: String){
-        val string = date;
-
-        val secondApiFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
-        val timestamp = 1565209665.toLong() // timestamp in Long
+    fun loadState(imageView: ImageView, status: String) {
+//        var requestOptions = RequestOptions()
+//        requestOptions = requestOptions.transforms(CenterCrop(), RoundedCorners(30))
 
 
-        val timestampAsDateString = DateTimeFormatter.ISO_INSTANT
-            .format(java.time.Instant.ofEpochSecond(timestamp))
+        val status = status
+        var statusImgPath: String = ""
 
-        Log.d("parseTesting", timestampAsDateString) // prints 2019-08-07T20:27:45Z
+        when(status) {
+            "PRS0101" -> statusImgPath = getURLForResource(R.drawable.prs0101) // 오픈예정 - jhm 2022/07/17
+            "PRS0102" -> statusImgPath = getURLForResource(R.drawable.prs0102) // 판매중 - jhm 2022/07/17
+            "PRS0103" -> statusImgPath = getURLForResource(R.drawable.prs0103) // 조각 완판 - jhm 2022/07/17
+            "PRS0104" -> LogUtil.logE("매각 대기")
+            "PRS0105" -> LogUtil.logE("매각 진행")
+            "PRS0106" -> statusImgPath = getURLForResource(R.drawable.prs0106) // 매각 완료 - jhm 2022/07/17
+            "PRS0107" -> LogUtil.logE("정산중")
+            "PRS0108" -> LogUtil.logE("분배완료")
+            "PRS0109" -> statusImgPath = getURLForResource(R.drawable.prs0109) // 일시 중지 - jhm 2022/07/17
+            "PRS0110" -> statusImgPath = getURLForResource(R.drawable.prs0110) // 기한 만료 - jhm 2022/07/17
+            "PRS0111" -> statusImgPath = getURLForResource(R.drawable.prs0111) // 수익 분배 - jhm 2022/07/17
+        }
 
 
-        val date = LocalDate.parse(timestampAsDateString, secondApiFormat)
-
-        Log.d("parseTesting", date.dayOfWeek.toString()) // prints Wednesday
-        Log.d("parseTesting", date.month.toString()) // prints August
-
-        textView.text = string
+        Glide.with(imageView.context)
+            .load(statusImgPath)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(imageView)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    @BindingAdapter("app:recruitmentBeginDate","app:recruitmentState")
+    @JvmStatic
+    fun loadData(textView: TextView, date: String ,status: String){
+        val test = status;
+
+
+
+        LogUtil.logE("status "+test)
+        val string = date;
+
+        LogUtil.logE("string " + string)
+        val year = string.substring(0,4)
+        val month = string.substring(5,7)
+        val day = string.substring(8,10)
+        val hour = string.substring(11,13)
+        val minute = string.substring(14,16)
+
+        val date: LocalDate = LocalDate.of(year.toInt(), month.toInt(), day.toInt())
+        val dayOfWeek: DayOfWeek = date.dayOfWeek
+
+        val dayFormat = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.KOREA)
+        val newDate = month + "." + day + "(" + dayFormat + ")" + hour + ":" + minute + " 오픈"
+        textView.text = newDate
+    }
+
+
+    private fun getURLForResource(resId: Int): String {
+        return Uri.parse("android.resource://" + R::class.java.getPackage().getName() + "/" + resId)
+            .toString()
+    }
 
 
 }
